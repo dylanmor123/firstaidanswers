790bd41943447d4c84ff968464df413ebde64e87
Dylan Ong <Dylan Ong>
2017-05-10 17:53:22 -0500

    small changes`

--- Answering.py
+++ Answering.py
@@ -7,17 +7,20 @@ VIDEO_CATEGORIES = ["What if", "Should I", "What should I do if", "How do I", "H
 
 IMAGE_CATEGORIES = ["What is", "What are the signs and symptoms of"]
 
-def Answer():
-	question = ""
+def Answer(question):
+	
 	while question != "OK":
-		question = raw_input("What's Your Emergency: ")
 		for category in IMAGE_CATEGORIES:
 			if category in question:
 				webbrowser.open(SearchImage(question))
+				question = raw_input("What's Your Emergency: ")
+
 
 		for category in VIDEO_CATEGORIES:
 			if category in question:
 				webbrowser.open(SearchYouTube(question))
+				question = raw_input("What's Your Emergency: ")
+
 	return
 
 
